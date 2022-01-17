ae8ce24875a4878666d7254a219618a427ad05e0
Fatih <curaat79@gmail.com>
2022-01-17 14:32:12 +0300

    Adding Message POJO class

---
+++ src/main/java/com/spirng/jms/model/HelloWorldMessage.java
@@ -0,0 +1,23 @@
+package com.spirng.jms.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 3296164102435655081L;
+
+    private UUID id;
+    private String message;
+
+}
